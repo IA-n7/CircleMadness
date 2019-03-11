6e0e9ad27a3bb5d77608ea88447a3bf35527ab03
IA-n7 <ian.e.w.bradley@gmail.com>
2019-03-11 11:12:28 -0400

    Wasn't commiting, bad practice. Almost finished game, polishing up game mechanics and adding in modes

--- Circles.js
+++ Circles.js
@@ -1,116 +1,343 @@
-// STATE VARIABLES
-var gameActive = 0;
-var smallCount = 0;
-var mediumCount = 0;
-var largeCount = 0;
-var timePassed;
+jQuery( document ).ready(function() {
 
-// ACTIVATE GAME
-const play = e => {
-  e.preventDefault();
-  gameActive = 1;
+  /*
+  STATE GLOBALS
+   */
+
+  var gameActive = 0;
+  var gamesPlayed = 0;
+  var madnessMode = 0;
+
+  var smallCount = 0;
+  var smallClicked = 0;
+  var mediumCount = 0;
+  var mediumClicked = 0;
+  var largeCount = 0;
+  var largeClicked = 0;
+
+  const smallValue = 3;
+  const mediumValue = 2;
+  const largeValue = 1;
+
+  var circleLifetime = 2000;
+
+  var score = 0;
+  var time = 0;
+  var timerId = 0;
+  var timerId2 = 0; //not in use right now
+  var interval = 1000; // not in use right now
+
+  // CREATE ALL INFORMATION NODES AND APPEND ELEMENT
+  var timeNode = document.createTextNode('0');
+  document.getElementById('timer').appendChild(timeNode);
+  var scoreNode = document.createTextNode('0');
+  document.getElementById('score').appendChild(scoreNode);
+  var timeLogNode = document.createTextNode('0');
+  document.getElementById('time').appendChild(timeLogNode);
+  var scoreLogNode = document.createTextNode('0');
+  document.getElementById('scored').appendChild(scoreLogNode);
+  var clicksLogNode = document.createTextNode('0');
+  document.getElementById('clicked').appendChild(clicksLogNode);
+  var misclicksLogNode = document.createTextNode('0');
+  document.getElementById('missed').appendChild(misclicksLogNode);
+
+  // var width = window.innerWidth;
+  // var height = window.innerHeight;
+
+  var modalArea = document.getElementById("modal-area");
+  var modalPlay = document.getElementById("modal-play");
+  var modalScore = document.getElementById("modal-score");
+  var modalOverlay = document.getElementById("modal-overlay");
+
+  /*
+  HELPER FUNCTIONS
+   */
+
+  // PROTOTYPE DEFINITIONS FOR REMOVING ELEMENTS
+  Element.prototype.remove = function() {
+      this.parentElement.removeChild(this);
+  }
+  NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+      for(var i = this.length - 1; i >= 0; i--) {
+          if(this[i] && this[i].parentElement) {
+              this[i].parentElement.removeChild(this[i]);
+          }
+      }
+  }
+
+  // ADJUSTMENT FUNCTION FOR WINDOW SIZE
+  // const onresize = e => {
+  //    width = e.target.innerWidth;
+  //    height = e.target.innerHeight;
+  // }
+
+  // RANDOM %-NUMBER FUNCTION
+  function randomNum () {
+    return Math.floor(Math.random() * 100);
+  }
+
+  // RANDOM ID-NUMBER FUNCTION
+  function randomID () {
+    let id = "";
+    let allPossibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 5; i++)
+      id += allPossibleCharacters.charAt(Math.floor(Math.random() * allPossibleCharacters.length));
+    return id;
+  }
+
   // REINITIALIZE STATE VARIABLES
-  smallCount = 0;
-  mediumCount = 0;
-  largeCount = 0;
-}
-
-// DEACTIVATE GAME
-function gameEnd () {
-  // RESET GAME STATE VARIABLE
-  gameActive = 0;
-}
-
-// CIRCLE GENERATION FUNCTIONS
-function smallCircle () {
-  // CREATE CIRCLE ELEMENT
-  let small = document.createElement("span");
-  // ADD CLASS TO CIRCLE
-  smallClass = `small${smallCount}`
-  small.className = smallClass;
-  // INSERT CIRCLE + INCREASE COUNT
-  let field = document.getElementById("field");
-  document.body.appendChild(small, field);
-  smallCount +=1;
-}
-
-function mediumCircle () {
-  // CREATE CIRCLE ELEMENT
-  let medium = document.createElement("span");
-  // ADD CLASS TO CIRCLE
-  mediumClass = `medium${mediumCount}`
-  medium.className = mediumClass;
-  // INSERT CIRCLE + INCREASE COUNT
-  let field = document.getElementById("field");
-  document.body.appendChild(medium, field);
-  mediumCount +=1;
-}
-
-function largeCircle () {
-  // CREATE CIRCLE ELEMENT
-  let large = document.createElement("span");
-  // ADD CLASS TO CIRCLE
-  largeClass = `large${largeCount}`
-  large.className = largeClass;
-  // INSERT CIRCLE + INCREASE COUNT
-  let field = document.getElementById("field");
-  document.body.appendChild(large, field);
-  largeCount +=1;
-}
-
-// TIMING FUNCTION
-function timing () {
-  window.setInterval((function(){
-     let start = Date.now();
-     timePassed = document.createTextNode('0');
-     document.getElementById('timer').appendChild(timePassed);
-     return function() {
-          timePassed.data = Math.floor((Date.now()-start)/1000);
-          };
-     }()), 1000);
-}
-
-// CIRCLES PER SECOND FUNCTION
-function generateCircles(circlesPerSecond) {
-  
-}
-
-// ADDING EVENT LISTENER TO PLAY BUTTONS
-let playBtn= document.getElementById('play-button');
-playBtn.addEventListener('click', function(event) {
-  event.preventDefault();
-
-  // INITIALIZE GAME
-  play();
-
-  // INITIALIZE GAME TIMER
-  timing();
-
-  // GAME
-  // FIRST LEVEL, LARGE CIRCLES ONLY (EASY)
-  if (timePassed >= 20) {
-
-  }
-  // SECOND LEVEL, LARGE AND MEDIUM CIRCLES (NORMAL)
-  if (timePassed >= 40) {
-
-  }
-  // THIRD LEVEL, ALL CIRCLES (HARD)
-  if (timePassed >= 60) {
-
-  }
-  // FOURTH LEVEL, MEDIUM AND SMALL CIRCLES (DIFFICULT)
-  if (timePassed >= 80) {
-
-  }
-  // FIFTH LEVEL, SMALL CIRCLES ONLY (EXTREME)
-  if (timePassed >= 100) {
-
+  function resetGlobals () {
+    smallCount = 0;
+    mediumCount = 0;
+    largeCount = 0;
+    smallClicked = 0;
+    mediumClicked = 0;
+    largeClicked = 0;
+    time = 0;
+    score = 0;
+    timeNode.data = 0;
+    scoreNode.data = 0;
+    timeLogNode.data = 0;
+    scoreLogNode.data = 0;
+    clicksLogNode.data = 0;
+    misclicksLogNode.data = 0;
   }
 
-  // END GAME
-  gameEnd();
-});
+  // CALCULATE SCORE
+  function scoring () {
+    scoreNode.data = (smallClicked * smallValue) + (mediumClicked * mediumValue) + (largeClicked * largeValue);
+    return true;
+  }
+
+  // REMOVE CIRCLES (CLEAR GAME FIELD)
+  function clearCircles () {
+    document.getElementsByClassName("largeCircle").remove();
+    document.getElementsByClassName("mediumCircle").remove();
+    document.getElementsByClassName("smallCircle").remove();
+  }
+
+  /*
+  CIRCLE GENERATION FUNCTIONS
+   */
+
+  function smallCircle () {
+    // CREATE CIRCLE ELEMENT
+    let small = document.createElement("div");
+    // ADD STYLING CLASS
+    small.className = 'smallCircle';
+    // SET RANDOM TOP & BOTTOM STYLES
+    let top = randomNum();
+    let left = randomNum();
+    // BOUNDARY CHECKS TO KEEP CIRCLE WITHIN GAME FIELD
+    if (top > 98.7) {top = 98.7;}
+    if (top < 0.3) {top = 0.3;}
+    if (left > 99) {left = 99;}
+    if (left < 0.2) {left = 0.2;}
+    small.setAttribute("style", `top:${top}%; left:${left}%;`);
+    // INSERT CIRCLE + INCREASE COUNT
+    let field = document.getElementById("field");
+    field.appendChild(small);
+    smallCount++;
+    // ADD EVENT LISTENER TO ELEMENT
+    small.addEventListener('click', function(event) {
+      // INCREMENT CLICKED ELEMENTS AND UPDATE SCORE
+      smallClicked++;
+      scoring();
+      // REMOVE ELEMENT
+      small.parentNode.removeChild(small);
+      return true;
+    });
+    if (!madnessMode) {
+      setTimeout(function(){
+        small.parentNode.removeChild(small);
+      }, circleLifetime);
+    }
+    return true;
+  }
+
+  function mediumCircle () {
+    // CREATE CIRCLE ELEMENT
+    let medium = document.createElement("div");
+    // ADD STYLING CLASS
+    medium.className = 'mediumCircle';
+    // SET RANDOM TOP & BOTTOM STYLES
+    let top = randomNum();
+    let left = randomNum();
+    // BOUNDARY CHECKS TO KEEP CIRCLE WITHIN GAME FIELD
+    if (top > 97.5) {top = 97.5;}
+    if (top < 0.3) {top = 0.3;}
+    if (left > 98) {left = 98;}
+    if (left < 0.2) {left = 0.2;}
+    medium.setAttribute("style", `top:${top}%; left:${left}%;`);
+    // INSERT CIRCLE + INCREASE COUNT
+    let field = document.getElementById("field");
+    field.appendChild(medium);
+    mediumCount++;
+    // ADD EVENT LISTENER TO ELEMENT
+    medium.addEventListener('click', function(event) {
+      // INCREMENT CLICKED ELEMENTS AND UPDATE SCORE
+      mediumClicked++;
+      scoring();
+      // REMOVE ELEMENT
+      medium.parentNode.removeChild(medium);
+      return true;
+    });
+    if (!madnessMode) {
+      setTimeout(function(){
+        medium.parentNode.removeChild(medium);
+      }, circleLifetime);
+    }
+    return true;
+  }
+
+  function largeCircle () {
+    // CREATE CIRCLE ELEMENT
+    let large = document.createElement("div");
+    // ADD STYLING CLASS
+    large.className = 'largeCircle';
+    // SET RANDOM TOP & BOTTOM STYLES
+    let top = randomNum();
+    let left = randomNum();
+    // BOUNDARY CHECKS TO KEEP CIRCLE WITHIN GAME FIELD
+    if (top > 96.3) {top = 96.3;}
+    if (top < 0.3) {top = 0.3;}
+    if (left > 97.2) {left = 97.2;}
+    if (left < 0.2) {left = 0.2;}
+    large.setAttribute("style", `top:${top}%; left:${left}%;`);
+    // INSERT CIRCLE + INCREASE COUNT
+    let field = document.getElementById("field");
+    field.appendChild(large);
+    largeCount++;
+    // ADD EVENT LISTENER TO ELEMENT
+    large.addEventListener('click', function(event) {
+      // INCREMENT CLICKED ELEMENTS AND UPDATE SCORE
+      largeClicked++;
+      scoring();
+      // REMOVE ELEMENT
+      large.parentNode.removeChild(large);
+      return true;
+    });
+    // REMOVE CIRCLE AFTER circleLifetime
+    if (!madnessMode) {
+      setTimeout(function(){
+        large.parentNode.removeChild(large);
+      }, circleLifetime);
+    }
+    return true;
+  }
+
+  /*
+  GAME FUNCTIONS
+   */
+
+  // ACTIVATE GAME
+  function play () {
+    gameActive = 1;
+    // CLEAR GAME FIELD FOR MADNESS MODE
+    if (madnessMode) {
+      clearCircles();
+    }
+    // REINITIALIZE STATE VARIABLES
+    resetGlobals();
+    // CLOSE MODALS
+    if (gamesPlayed === 0) {
+      modalPlay.classList.add("hidden");
+      modalOverlay.classList.add("hidden");
+      modalArea.classList.add("hidden");
+    } else {
+      modalScore.classList.add("hidden");
+      modalOverlay.classList.add("hidden");
+      modalArea.classList.add("hidden");
+    }
+    // INITIALIZE GAME
+    game();
+    return true;
+  }
+
+  // DEACTIVATE GAME
+  function gameEnd () {
+    // RESET GAME STATE AND INCREMENT GAMES PLAYED
+    gameActive = 0;
+    gamesPlayed++;
+    // STOP TIMER
+    clearInterval(timerId);
+    // LOG ALL PLAY DATA IN SCORE MODAL
+    timeLogNode.data = time;
+    scoreLogNode.data = score;
+    clicksLogNode.data = smallClicked + mediumClicked + largeClicked;
+    misclicksLogNode.data = (smallCount - smallClicked) + (mediumCount - mediumClicked) + (largeCount - largeClicked);
+    // OPEN SCORE MODAL
+    modalScore.classList.remove("hidden");
+    modalOverlay.classList.remove("hidden");
+    modalArea.classList.remove("hidden");
+    return true;
+  }
+
+  // GAMEPLAY FUNCTION
+  function game () {
+    // TIMING INTERVAL FUNCTION
+    timerId = window.setInterval((function(){
+      // GAME CONDITIONS
+      return function() {
+        // SET timeNode AND INCREMENT GAME TIME
+        timeNode.data = time;
+        time++;
+
+        // FIRST LEVEL, LARGE CIRCLES ONLY (EASY)
+        if (time <= 15) {
+          largeCircle();
+        }
+        // SECOND LEVEL, LARGE AND MEDIUM CIRCLES (NORMAL)
+        if (time > 15 && time <= 30) {
+          mediumCircle();
+        }
+        // THIRD LEVEL, ALL CIRCLES (HARD)
+        if (time > 30 && time <= 45) {
+          smallCircle();
+        }
+        // FOURTH LEVEL, MEDIUM AND SMALL CIRCLES (DIFFICULT)
+        if (time > 45 && time <= 60) {
+          gameEnd();
+        }
+        // FIFTH LEVEL, SMALL CIRCLES ONLY (EXTREME)
+        if (time > 60 && time <= 80) {
+
+        }
+        // END GAME
+        if (time >= 100) {
+
+        }
+
+      };
+    }()), 1000);
+  }
+
+  /*
+  EVENT LISTENERS
+   */
+
+  // ADDING EVENT LISTENER TO WINDOW FOR RESIZING
+  // window.addEventListener("resize", onresize);
+
+  // ADDING EVENT LISTENER TO PLAY BUTTON [MODAL-PLAY]
+  let playBtn = document.getElementById('play-button');
+  playBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    // INITIALIZE GAME
+    play();
+    return true;
+  });
+
+  // ADDING EVENT LISTENER TO PLAY BUTTON [MODAL-SCORE]
+  let playBtn2 = document.getElementById('play-button2');
+  playBtn2.addEventListener('click', function(event) {
+    event.preventDefault();
+    // INITIALIZE GAME
+    play();
+    return true;
+  });
+
+}); // END DOCUMENT READY
 
 
 
@@ -125,8 +352,4 @@ playBtn.addEventListener('click', function(event) {
 
 
 
-
-
-
-
-// WORKSPACE
+  // WORKSPACE
